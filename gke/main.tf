a5a742ea11984532ea011608b441578d851c2aa9
aliiazhanybekova <alex.zhanybekova@gmail.com>
2024-04-27 13:29:42 -0400

    FIXED

--- gke/main.tf
+++ gke/main.tf
@@ -1,9 +1,10 @@
 module "gke_cluster" {
   source                       = "github.com/aliiazhanybekova/tf-modules"
-  service_account_id           = "gke-service-account"
-  service_account_display_name = "GKE Service Account"
+  service_account_id           = "gke-service-account21"
+  service_account_display_name = "SERVICE ACCOUNT"
   cluster_name                 = "dev-cluster"
   cluster_location             = "us-central1-a"
   project_id                   = "my-project2-421005"
+  bucket_name                  = "tfstate_bucket3"
   
 }
